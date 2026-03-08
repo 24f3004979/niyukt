347bf8bd3c2d04d9ab6ca198ea163e9d4e5998ff
madhavprapanna52 <madhavprapanna@gmail.com>
2026-03-08 15:55:42 +0530

    Multi anchor system making query

--- endpoint/QueryBuilder.py
+++ endpoint/QueryBuilder.py
@@ -23,15 +23,42 @@ class QueryBuilder:
         print(f"Length for the placeholder : {type(self.columns)}")
         query = f'insert into {self.table}{self.columns} values({placeholder})'
         print(f"Query for Insertion : {query} More debug : placeholder : {placeholder} with col : {self.columns}")
-        return query  # insertion Query 
+        return query  # insertion Query
 
     def edit(self,edit_information, anchor_information):
         col = edit_information[0] # colum to edit
         query = f"update {self.table} set {col} = ? where {anchor_information[0]} = ?"
         data = (edit_information[1], anchor_information[1])
         return query, data  # TODO : Extend for mutliple anchor information
+    
+    def selection(self, anchor_information, required_columns):
+        '''
+        anchor_ioformation -> [(), ()]
+        List for fetching information with multiple [AND] based anchored fetch request
+        '''
+        base_query = f"select {required_columns} from {self.table} where "
+        print(f'Base Query : {base_query}')
 
+        i = 0  # placeholder constraints
+        data = []  # Final Execution data list
+        condition_strings = []
+        for anchor_elem in anchor_information:
+            elem = f'({anchor_elem[0]} = ?{i})'
+            condition_strings.append(elem)
+            data.append(anchor_elem[1])
+            i += 1
 
+        condition_string = " and ".join(condition_strings)
+        query = base_query + condition_string
 
+        print(f"Query : {query} with data \n : {data}")
+        return query, data
+
+column = "name,email,password,detail".split(",")
+q = QueryBuilder("user", column)
+
+anchor_information = [("name", "Madhav"), ("password", "11234")]
+required_columns = "name,password"
+q.selection(anchor_information, required_columns)
 
 
